7aee526677f431c037d48ae3941347af8db3a6f0
Badduluri Nithin kumar <nithinkumarnk7893@gmail.com>
2024-12-26 15:34:52 +0530

    Add files via upload

---
+++ PrimitiveDtype.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Primitive
+{
+public static void main(String args[])
+{
+Scanner s=new Scanner(System.in);
+int a;
+System.out.println("enter the value:");
+a=s.nextInt();
+System.out.println("value of a is="+a);
+}
+}
