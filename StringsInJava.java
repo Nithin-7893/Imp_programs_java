c7df9d96bcd32f0ee4f1e2397873b7727cff3c10
Badduluri Nithin kumar <nithinkumarnk7893@gmail.com>
2024-12-26 15:40:29 +0530

    Add files via upload

---
+++ StringsInJava.java
@@ -0,0 +1,12 @@
+class Familymember
+{
+public static void main(String args[])
+{
+char a,b,c,d;
+a="nithin";
+b="akash";
+c="ramesh";
+d="sarasa";
+System.out.printf(" ***PRINTING NAMES OF FAMILY MEMBERS***\na=%c\nb=%c\nc=%c\nd=%c",a,b,c,d);
+}
+}
