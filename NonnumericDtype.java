8db87c9b9e12f14f845b764d51aa5ae8051cb1ba
Badduluri Nithin kumar <nithinkumarnk7893@gmail.com>
2024-12-26 15:37:39 +0530

    Add files via upload

---
+++ NonnumericDtype.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class Nonnumeric
+{
+public static void main(String args[])
+{
+boolean a;
+char b;
+Scanner w =new Scanner(System.in);
+System.out.println("enter non-numeric data type values:");
+a=w.nextBoolean();
+//b.nextChar();                                                                                                                                                              
+System.out.println("boolean value a="+a);
+//System.out.println("character value b="+b);                                     
+}
+}                      
