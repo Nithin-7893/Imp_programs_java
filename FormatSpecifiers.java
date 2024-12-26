d511c01c547e66d6dc1f5eab6732234de7270611
Badduluri Nithin kumar <nithinkumarnk7893@gmail.com>
2024-12-26 15:39:09 +0530

    Add files via upload

---
+++ FormatSpecifiers.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Multiple
+{
+public static void main(String args[])
+{
+Scanner s=new Scanner(System.in);
+int a,b,c,d;
+System.out.println("enter the values:");
+a=s.nextInt();
+b=s.nextInt();
+c=s.nextInt();
+d=s.nextInt();
+System.out.printf(" ****PRINTING VERTICALLY****\na=%d\nb=%d\nc=%d\nd=%d",a,b,c,d);
+System.out.printf("****PRINTING HORIZONTALLY****\na=%d\tb=%d\tc=%d\td=%d",a,b,c,d);
+}
+}
